b589e7fef95e67d66589403ab04a7ec804b7d2fe
achimmft <70777695+achimmft@users.noreply.github.com>
2024-04-02 17:21:48 -0600

    complete listing page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +8,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false
-      }
-    }
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-})
+});
