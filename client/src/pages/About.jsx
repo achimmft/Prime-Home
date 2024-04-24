c6dcda5a161060203a2b6da37ce7ad1d9d5cad06
achimmft <70777695+achimmft@users.noreply.github.com>
2024-04-24 17:29:38 -0600

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Welcome to PrimeHome, Your Ultimate Real Estate Solution!</h1>
+      <p className="mb-4 text-slate-700">
+        At PrimeHome, we're passionate about simplifying your real estate
+        journey. Whether you're buying, selling, or renting property, our
+        platform is designed to provide a seamless experience. With PrimeHome,
+        you have access to a wide range of listings, advanced search filters,
+        and powerful tools to help you find your dream home or investment
+        property.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission at PrimeHome is to empower both buyers and sellers in the
+        real estate market. For buyers, we offer detailed property listings,
+        virtual tours, and neighborhood insights, giving you all the information
+        you need to make informed decisions. Sellers benefit from our innovative
+        marketing tools, including professional photography, virtual staging,
+        and targeted advertising, to showcase their properties to a wider
+        audience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        PrimeHome is more than just a real estate platform - it's a community.
+        Our team of dedicated professionals is here to support you every step of
+        the way, from your initial search to closing the deal. Whether you're a
+        first-time homebuyer, seasoned investor, or looking to rent your next
+        apartment, PrimeHome is your trusted partner in real estate. Join us
+        today and let's find your perfect home together!
+      </p>
     </div>
-  )
+  );
 }
